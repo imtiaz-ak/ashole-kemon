1a808b37317d66deff8c5f00b0f4c857a4e80d1e
Imtiaz Ahmed Khan <ak.imtiaz147@gmail.com>
2022-01-25 09:39:28 +0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import SearchBox from '../components/SearchBox'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
